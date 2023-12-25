a3e6027d31a82ccb442747065a1f2b4aa7251211
Erkan mert Kılavuz <2314506003@ogr.mehmetakif.edu.tr>
2023-12-25 19:22:27 +0300

    fixed Ses User Control

--- Ses.cs
+++ Ses.cs
@@ -16,5 +16,33 @@ namespace Proje
         {
             InitializeComponent();
         }
+
+        #region Properties
+        private Button _Button;
+        private string _Name;
+        private Image _Icon;
+
+        [Category("Ses Props")]
+        public string SesName
+        {
+            get { return _Name; }
+            set { _Name = value; label1.Text = value; }
+        }
+
+        [Category("Ses Props")]
+        public Button Button
+        {
+            get { return _Button; }
+            set { _Button = value; button1 = value; }
+        }
+
+        [Category("Ses Props")]
+        public Image Icon
+        {
+            get { return _Icon; }
+            set { _Icon = value; pictureBox1.Image = value; }
+        }
+        #endregion
+
     }
 }
